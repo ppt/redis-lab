345ebbd333c732492e876e560e7d2838ff55ba24
ppt <theepraphan@gmail.com>
2016-05-15 11:12:12 +0700

    refactor feeder.js

--- feeder.js
+++ feeder.js
@@ -1,8 +1,14 @@
 "use strict";
 
+// /seqNo[":\s]+(\w+)/i.exec(s)[1]
+// /mesgType[":\s]+(\w+)/i.exec(s)[1]
+// /}\s*}\s*$/.exec(s)
+// /^{[^{]+({.*})\s*}\*$/.exec(s)[1]
+
 var numeral = require("numeral"),
     moment = require("moment"),
     redis = require("redis"),
+    ioredis = require("ioredis"),
     now = moment(),
     filename = "/Users/praphan/Desktop/projects/Redis/md-message1-20160308.log",
     // filename = "/Users/praphan/Desktop/projects/Redis/100lines.json",
@@ -10,7 +16,7 @@ var numeral = require("numeral"),
     loopsize = 1000,
     datastr = '';
 
-function save(data) {
+function save_redis(data) {
     var client = redis.createClient("/tmp/redis.sock"),
         m = client.multi(),
         id = cnt - data.length + 1;
@@ -31,36 +37,66 @@ function save(data) {
     client.quit();
 }
 
-require("readline").createInterface({
-    input: require("fs").createReadStream(filename)
-}).on("line", function(line) {
-    cnt = cnt + 1;
+function save_ioredis(data) {
+    var client = new ioredis("/tmp/redis.sock"),
+        m = client.multi(),
+        id = cnt - data.length + 1;
+    for (var i = 0; i < data.length; i++) {
+        var keys = Object.keys(data[i]),
+            s = '';
+        if (keys.length > 2) {
+            s = JSON.stringify(data[i][keys[2]]);
+        }
+        m.hmset(
+            `logs:${id + i}`,
+            "seqNo", data[i].seqNo,
+            "mesgType", data[i].mesgType,
+            "msg", s
+        );
+    }
+    m.exec();
+    client.quit();
+}
 
+function process_line_JSONParse(line) {
     if (cnt % loopsize == 1) {
         datastr = '[' + line;
     } else {
         datastr = datastr + ',' + line;
     }
+}
 
-    if (cnt % loopsize == 0) {
-        save(JSON.parse(datastr + ']'));
-        datastr = null;
-    }
+function save_JSONParse() {
+    save_redis(JSON.parse(datastr + ']'));
+}
 
-
-    // save(data,line);
-}).on("close", function() {
-    if (datastr) {
-        save(JSON.parse(datastr + ']'));
-    }
-    var client = redis.createClient("/tmp/redis.sock");
-    client.set('logs:size', cnt);
-    client.quit();
-
-    console.log("line = " + numeral(cnt).format());
-    console.log("chunk = " + loopsize);
-    console.log("time = " + moment(moment().diff(now)).format("mm:ss.SSS"));
+function load(process_line, save) {
     console.log(moment().format('HH:mm:ss.SSS'));
-});
 
-console.log(moment().format('HH:mm:ss.SSS'));
+    require("readline").createInterface({
+        input: require("fs").createReadStream(filename)
+    }).on("line", function(line) {
+        cnt = cnt + 1;
+        process_line(line);
+        if (cnt % loopsize == 0) {
+            // save_ioredis(JSON.parse(datastr + ']'));
+            save();
+            datastr = null;
+        }
+    }).on("close", function() {
+        if (datastr) {
+            save();
+        }
+        var client = redis.createClient("/tmp/redis.sock");
+        client.set('logs:size', cnt);
+        client.quit();
+
+        console.log("line = " + numeral(cnt).format());
+        console.log("chunk = " + loopsize);
+        console.log("time = " + moment(moment().diff(now)).format("mm:ss.SSS"));
+        console.log(moment().format('HH:mm:ss.SSS'));
+    });
+
+}
+// load_JSONParse();
+load(process_line_JSONParse, save_JSONParse);
